1e65dac7a69155579893d3c7e18fa5d03299eb73
Simran Kaur <simran.kaur@wbcomdesigns.in>
2025-05-29 15:30:55 +0530

    Updated plugin feedback link.
    https://3.basecamp.com/5798509/buckets/42374786/card_tables/cards/8672900655

--- admin/partials/wb-ajax-filter-welcome-page.php
+++ admin/partials/wb-ajax-filter-welcome-page.php
@@ -44,7 +44,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 						<div class="wbcom-support-inner">
 						<h3><span class="dashicons dashicons-admin-comments"></span><?php esc_html_e( 'Got Feedback?', 'wb-ajax-filter' ); ?></h3>
 						<p><?php esc_html_e( 'We want to hear about your experience with the plugin. We would also love to hear any suggestions you may for future updates.', 'wb-ajax-filter' ); ?></p>
-						<a href="<?php echo esc_url( 'https://wbcomdesigns.com/contact/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Send Feedback', 'wb-ajax-filter' ); ?></a>
+						<a href="<?php echo esc_url( 'https://wbcomdesigns.com/submit-review/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Send Feedback', 'wb-ajax-filter' ); ?></a>
 					</div>
 					</div>
 				</div>
