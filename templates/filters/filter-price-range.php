6943cd0996cef23fff6a7b9dad7cc57acd30834e
Simran Kaur <simran.kaur@wbcomdesigns.in>
2025-05-29 16:05:03 +0530

    Resolved php warning related to toggle_style.
    https://3.basecamp.com/5798509/buckets/42374786/card_tables/cards/8682382581

--- templates/filters/filter-price-range.php
+++ templates/filters/filter-price-range.php
@@ -35,7 +35,7 @@ $wb_ajax_filter_general_options = get_option( 'wb_ajax_filter_admin_general_opti
 		<span class="dashicons <?php echo esc_attr( $toggle_icon ); ?>"></span>
 		<?php endif; ?>
 	</a>
-	<div class="wb-ajax-panel" style="<?php echo ( $toggle_enabled && 'closed' === $filters['toggle_style'] ) ? 'display:none' : ''; ?>">
+	<div class="wb-ajax-panel" style="<?php echo esc_attr( $toggle_style ); ?>">
 		<?php if ( isset( $wb_ajax_filter_general_options['show_clear_filter'] ) && 'yes' === $wb_ajax_filter_general_options['show_clear_filter'] ) { ?>
 		<a href="javascript:void(0)" class="wb-ajax-clear-single-filter" data-filter="min_price,max_price" style="<?php echo ( isset( $_GET['min_price'] ) ) ? '' : esc_attr( $clear_style ); //phpcs:ignore?>"><?php esc_html_e( 'Clear', 'wb-ajax-filter' ); ?></a>
 		<?php } ?>
