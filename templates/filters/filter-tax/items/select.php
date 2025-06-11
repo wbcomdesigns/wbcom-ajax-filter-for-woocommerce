2328a7507b63ff536fae2e7aaed8754bf4acf042
Simran Kaur <simran.kaur@wbcomdesigns.in>
2025-06-11 12:36:00 +0530

    Implemented selected term productcount in select box.
    https://3.basecamp.com/5798509/buckets/42374786/card_tables/cards/8682438659

--- templates/filters/filter-tax/items/select.php
+++ templates/filters/filter-tax/items/select.php
@@ -13,15 +13,18 @@ $hide_term = ( isset( $wb_ajax_filter_general_options['hide_empty_terms'] ) && '
 ?>
 <div class="wb-ajax-filter filter-tax">
 	<select class="wb-ajax-filter-selectible" data-filter="<?php echo esc_attr( $filter_taxonomy ); ?>" <?php echo ( isset( $filters['multiple'] ) && 'yes' === $filters['multiple'] ) ? 'multiple="multiple"' : ''; ?>>
-		<option value=""><?php esc_html_e( 'Choose term', 'wb-ajax-filter' ); ?></option>
+		<option value=""><?php esc_html_e( 'Select term', 'wb-ajax-filter' ); ?></option>
 		<?php
-		foreach ( $filters['terms'] as $tm ) {
-			$term_data = get_term( $tm->id, $filters['taxonomy'] );
+		foreach ( $filters['terms'] as $term ) {
+			$term_data = get_term( $term->id, $filters['taxonomy'] );
 			$disabled  = ( 0 === $term_data->count ) ? 'disabled' : '';
 			if ( ( $hide_term && '' === $disabled ) || ( ! $hide_term && '' === $disabled ) || ( ! $hide_term && 'disabled' === $disabled ) ) {
 				?>
-			<option value="<?php echo esc_attr( $term_data->slug ); ?>" <?php echo ( isset( $params[ $filter_taxonomy ] ) && $term_data->slug === $params[ $filter_taxonomy ] ) ? 'selected' : ''; ?> <?php echo esc_attr( $disabled ); ?>><?php echo esc_attr( $term_data->name ); ?></option>
-				<?php
+			<option value="<?php echo esc_attr( $term_data->slug ); ?>" <?php echo ( isset( $params[ $filter_taxonomy ] ) && $term_data->slug === $params[ $filter_taxonomy ] ) ? 'selected' : ''; ?> <?php echo esc_attr( $disabled ); ?>><?php echo esc_attr( $term_data->name ); ?><?php if ( isset( $filters['show_count'] ) && 'yes' === $filters['show_count'] ) : ?>
+				<small class="item-count">(<?php echo esc_html( $term_data->count ); ?>)</small>
+				<?php endif; ?>
+			</option>
+			<?php
 			}
 		}
 		?>
